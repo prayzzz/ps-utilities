07693894d26afa28b3b961189a925a21e67dd8cf
prayzzz <prayzzz@outlook.com>
2017-06-23 18:40:18 +0200

    - improved removal

--- windows/remove-context-entries.ps1
+++ windows/remove-context-entries.ps1
@@ -8,30 +8,38 @@ function Remove-Registry-Entry ($path) {
     }
 }
 
+function Remove-Registry-Entry-By-Name ($basePath, $name) {
+    $entries = Get-ChildItem -Path $basePath -Recurse | Where-Object -FilterScript {$_.Name -like "*$name"}
+
+    if ($entries.Length -gt 1)
+    {
+        Write-Host "Found multiple entries:"
+        foreach($entry in $entries)
+        {
+            Write-Host $entry
+        }
+    }
+    elseif($entries.Length -eq 0)
+    {
+        Write-Host "Not found: $path"        
+    }
+    else
+    {
+        Remove-Registry-Entry "Registry::$($entries[0].Name)"
+    }
+}
+
 Write-Host "####"
 Write-Host "### Remove Context Entries Script"
 Write-Host "### Execute this script as Administrator"
 Write-Host "####"
 Write-Host
 
-$drives = Get-PSDrive -PSProvider Registry
-$hkcrfound = $false
-foreach ($drive in $drives) {
-    if ($drive.Name -eq "HKCR") {
-        $hkcrfound = $true
-    }
-}
-
-if (!$hkcrfound) {
-    Write-Host "HKCR not found. Adding..."
-    Write-Host ""
-
-    New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null
-}
-
-Remove-Registry-Entry "HKCR:\Directory\background\shell\git_shell"
-Remove-Registry-Entry "HKCR:\Directory\background\shell\git_gui"
-Remove-Registry-Entry "HKCR:\Directory\shell\git_shell"
-Remove-Registry-Entry "HKCR:\Directory\shell\git_gui"
+Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Directory\background\shell\git_shell"
+Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Directory\background\shell\git_gui"
+Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Directory\shell\git_shell"
+Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Directory\shell\git_gui"
+Remove-Registry-Entry-By-Name "Registry::HKEY_CLASSES_ROOT\CLSID\" "shell\Open CCleaner..."
+Remove-Registry-Entry-By-Name "Registry::HKEY_CLASSES_ROOT\CLSID\" "shell\Run CCleaner"
 
 Write-Host
