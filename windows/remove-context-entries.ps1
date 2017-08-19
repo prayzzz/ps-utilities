921854bc9e03a5fa5276cd9d9a3accf7781dd43c
prayzzz <prayzzz@outlook.com>
2017-08-19 23:01:54 +0200

    - added more context entries

--- windows/remove-context-entries.ps1
+++ windows/remove-context-entries.ps1
@@ -11,34 +11,41 @@ function Remove-Registry-Entry ($path) {
 function Remove-Registry-Entry-By-Name ($basePath, $name) {
     $entries = Get-ChildItem -Path $basePath -Recurse | Where-Object -FilterScript {$_.Name -like "*$name"}
 
-    if ($entries.Length -gt 1)
-    {
+    if ($entries.Length -gt 1) {
         Write-Host "Found multiple entries:"
-        foreach($entry in $entries)
-        {
+        foreach ($entry in $entries) {
             Write-Host $entry
         }
     }
-    elseif($entries.Length -eq 0)
-    {
-        Write-Host "Not found: $path"        
+    elseif ($entries.Length -eq 0) {
+        Write-Host "Not found: *$name"        
     }
-    else
-    {
+    else {
         Remove-Registry-Entry "Registry::$($entries[0].Name)"
     }
 }
 
 Write-Host "####"
-Write-Host "### Remove Context Entries Script"
+Write-Host "### Removes unwanted context menu entries"
 Write-Host "### Execute this script as Administrator"
 Write-Host "####"
 Write-Host
 
 Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Directory\background\shell\git_shell"
 Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Directory\background\shell\git_gui"
+Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Directory\Background\OpenWithMobaXterm"
+Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\AnyCode"
+
 Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Directory\shell\git_shell"
 Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Directory\shell\git_gui"
+Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Directory\shell\OpenWithMobaXterm"
+Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Directory\shell\PlayWithVLC"
+Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Directory\shell\AddToPlaylistVLC"
+Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Directory\shell\AnyCode"
+
+Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Drive\shell\OpenWithMobaXterm"
+Remove-Registry-Entry "Registry::HKEY_CLASSES_ROOT\Applications\vlc.exe\shell\Open"
+
 Remove-Registry-Entry-By-Name "Registry::HKEY_CLASSES_ROOT\CLSID\" "shell\Open CCleaner..."
 Remove-Registry-Entry-By-Name "Registry::HKEY_CLASSES_ROOT\CLSID\" "shell\Run CCleaner"
 
